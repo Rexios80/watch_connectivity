25c1c690cdd86e06dec41a25a157d5e039e845ef
Rexios <rexios@rexios.dev>
2022-02-02 15:42:36 -0500

    Removed wear example since it's unnecessary

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -17,19 +17,27 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   final _watch = WatchConnectivity();
 
-  bool _paired = false;
-  bool _reachable = false;
+  var _paired = false;
+  var _reachable = false;
+  var _context = <String, dynamic>{};
+  var _receivedContext = <String, dynamic>{};
+  final _log = <String>[];
 
   @override
   void initState() {
     super.initState();
     initPlatformState();
+
+    _watch.messageStream.listen((e) => _log.add('Received message: $e'));
+    _watch.contextStream.listen((e) => _log.add('Received context: $e'));
   }
 
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
     _paired = await _watch.isPaired;
     _reachable = await _watch.isReachable;
+    _context = await _watch.applicationContext;
+    _receivedContext = await _watch.receivedApplicationContext;
     setState(() {});
   }
 
@@ -37,18 +45,18 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
         body: Center(
-          child: Column(
+          child: ListView(
             children: [
               Text('Paired: $_paired'),
               Text('Reachable: $_reachable'),
+              Text('Context: $_context'),
+              Text('Received context: $_receivedContext'),
               ElevatedButton(
-                child: const Text('Check again'),
+                child: const Text('Refresh'),
                 onPressed: initPlatformState,
               ),
+              
             ],
           ),
         ),
--- example_wear/android/app/src/main/kotlin/dev/rexios/watch_connectivity_example_wear/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package dev.rexios.watch_connectivity_example_wear
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
