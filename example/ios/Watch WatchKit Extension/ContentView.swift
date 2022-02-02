0579c185cca70077a87c8da32bc51c218e0ee8f0
Rexios <rexios@rexios.dev>
2022-02-02 15:05:58 -0500

    Added watch target to iOS code

---
+++ example/ios/Watch WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Watch WatchKit Extension
+//
+//  Created by Aaron DeLory on 2/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ example/ios/Watch WatchKit Extension/RunnerApp.swift
@@ -0,0 +1,19 @@
+//
+//  RunnerApp.swift
+//  Watch WatchKit Extension
+//
+//  Created by Aaron DeLory on 2/2/22.
+//
+
+import SwiftUI
+
+@main
+struct RunnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
