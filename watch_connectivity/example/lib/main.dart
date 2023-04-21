450e60cee1bd9c156e8bf1a36063fd7a72259450
Rexios <rexios@rexios.dev>
2023-04-21 13:12:11 -0400

    Remove garmin from example

--- watch_connectivity/example/lib/main.dart
+++ watch_connectivity/example/lib/main.dart
@@ -1,11 +1,9 @@
 import 'dart:async';
-import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:is_wear/is_wear.dart';
 
 import 'package:watch_connectivity/watch_connectivity.dart';
-import 'package:watch_connectivity_garmin/watch_connectivity_garmin.dart';
 import 'package:wear/wear.dart';
 
 late final bool isWear;
@@ -26,7 +24,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late final WatchConnectivityBase _watch;
+  final _watch = WatchConnectivity();
 
   var _count = 0;
 
@@ -43,26 +41,11 @@ class _MyAppState extends State<MyApp> {
   void initState() {
     super.initState();
 
-    // Change this to the plugin you want to test.
-    // e.g. `_watch = WatchConnectivityGarmin();`
-    _watch = WatchConnectivity();
-
-    if (_watch is WatchConnectivityGarmin) {
-      (_watch as WatchConnectivityGarmin).initialize(
-        GarminInitializationOptions(
-          applicationId: 'daed64bf-ecee-4b75-b736-f0f834801d6a',
-          urlScheme: 'watch-connectivity-example',
-        ),
-      );
-    }
-
     _watch.messageStream
         .listen((e) => setState(() => _log.add('Received message: $e')));
 
-    if (_watch is! WatchConnectivityGarmin) {
-      _watch.contextStream
-          .listen((e) => setState(() => _log.add('Received context: $e')));
-    }
+    _watch.contextStream
+        .listen((e) => setState(() => _log.add('Received context: $e')));
 
     initPlatformState();
   }
@@ -72,10 +55,8 @@ class _MyAppState extends State<MyApp> {
     _supported = await _watch.isSupported;
     _paired = await _watch.isPaired;
     _reachable = await _watch.isReachable;
-    if (_watch is! WatchConnectivityGarmin) {
-      _context = await _watch.applicationContext;
-      _receivedContexts = await _watch.receivedApplicationContexts;
-    }
+    _context = await _watch.applicationContext;
+    _receivedContexts = await _watch.receivedApplicationContexts;
     setState(() {});
   }
 
@@ -92,20 +73,12 @@ class _MyAppState extends State<MyApp> {
                 Text('Supported: $_supported'),
                 Text('Paired: $_paired'),
                 Text('Reachable: $_reachable'),
-                if (_watch is! WatchConnectivityGarmin) ...[
-                  Text('Context: $_context'),
-                  Text('Received contexts: $_receivedContexts'),
-                ],
+                Text('Context: $_context'),
+                Text('Received contexts: $_receivedContexts'),
                 TextButton(
                   onPressed: initPlatformState,
                   child: const Text('Refresh'),
                 ),
-                if (_watch is WatchConnectivityGarmin && Platform.isIOS)
-                  TextButton(
-                    onPressed:
-                        (_watch as WatchConnectivityGarmin).showDeviceSelection,
-                    child: const Text('Open device selection'),
-                  ),
                 const SizedBox(height: 8),
                 const Text('Send'),
                 Row(
@@ -115,13 +88,11 @@ class _MyAppState extends State<MyApp> {
                       onPressed: sendMessage,
                       child: const Text('Message'),
                     ),
-                    if (_watch is! WatchConnectivityGarmin) ...[
-                      const SizedBox(width: 8),
-                      TextButton(
-                        onPressed: sendContext,
-                        child: const Text('Context'),
-                      ),
-                    ],
+                    const SizedBox(width: 8),
+                    TextButton(
+                      onPressed: sendContext,
+                      child: const Text('Context'),
+                    ),
                   ],
                 ),
                 TextButton(
@@ -130,6 +101,11 @@ class _MyAppState extends State<MyApp> {
                     '${timer == null ? 'Start' : 'Stop'} background messaging',
                     textAlign: TextAlign.center,
                   ),
+                ),
+                const SizedBox(width: 16),
+                TextButton(
+                  onPressed: _watch.startWatchApp,
+                  child: const Text('Start watch app'),
                 ),
                 const SizedBox(width: 16),
                 const Text('Log'),
