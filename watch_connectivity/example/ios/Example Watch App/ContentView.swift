0562378ee831403535b565a6202ea520b6450c1c
Rexios <rexios@rexios.dev>
2023-10-18 11:06:02 -0400

    Create new watch app

---
+++ watch_connectivity/example/ios/Example Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Example Watch App
+//
+//  Created by Rexios on 10/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
