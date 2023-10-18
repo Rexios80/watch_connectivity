14dccfab723a617a9805b6732597e1d9b5369e26
Rexios <rexios@rexios.dev>
2023-10-18 11:09:53 -0400

    Recreate UI

--- watch_connectivity/example/ios/Example Watch App/ContentView.swift
+++ watch_connectivity/example/ios/Example Watch App/ContentView.swift
@@ -6,19 +6,38 @@
 //
 
 import SwiftUI
+import WatchConnectivity
 
 struct ContentView: View {
+    @ObservedObject var session = WatchSessionDelegate()
+    @State var count = 0
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView {
+            Text("Reachable: \(session.reachable.description)")
+            Text("Context: \(session.context.description)")
+            Text("Received context: \(session.receivedContext.description)")
+            Button("Refresh") { session.refresh() }
+            Spacer().frame(height: 8)
+            Text("Send")
+            HStack {
+                Button("Message") { session.sendMessage(["data": "Hello"]) }
+                Button("Context") {
+                    count += 1
+                    session.updateApplicationContext(["data": count])
+                }
+            }
+            Spacer().frame(height: 8)
+            Text("Log")
+            ForEach(session.log.reversed(), id: \.self) {
+                Text($0)
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
