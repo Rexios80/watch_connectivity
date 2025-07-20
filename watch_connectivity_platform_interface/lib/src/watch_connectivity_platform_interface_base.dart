45b88ff00ad6dfb8c34459ea00ea085d6352534a
_Reasonable_Psycho_ <redkcz@gmail.com>
2025-07-20 23:48:50 +0200

    Fixed sending messages in WatchConnectivityGarmin.
    Removed the underscore from _messageStreamController to enable access inside WatchConnectivityGarmin.
    Added the appropriate message handling inside WatchConnectivityGarmin.

--- watch_connectivity_platform_interface/lib/src/watch_connectivity_platform_interface_base.dart
+++ watch_connectivity_platform_interface/lib/src/watch_connectivity_platform_interface_base.dart
@@ -13,33 +13,38 @@ abstract class WatchConnectivityBase {
   @protected
   final MethodChannel channel;
 
-  final _messageStreamController =
-      StreamController<Map<String, dynamic>>.broadcast();
-  final _contextStreamController =
-      StreamController<Map<String, dynamic>>.broadcast();
+  @protected
+  final messageStreamController =
+  StreamController<Map<String, dynamic>>.broadcast();
+
+  @protected
+  final contextStreamController =
+  StreamController<Map<String, dynamic>>.broadcast();
 
   /// Stream of messages received
+  @protected
   Stream<Map<String, dynamic>> get messageStream =>
-      _messageStreamController.stream;
+      messageStreamController.stream;
 
   /// Stream of contexts received
+  @protected
   Stream<Map<String, dynamic>> get contextStream =>
-      _contextStreamController.stream;
+      contextStreamController.stream;
 
   /// Create an instance of [WatchConnectivityBase] for the given
   /// [pluginName]
   WatchConnectivityBase({required String pluginName})
       : channel = MethodChannel(pluginName) {
-    channel.setMethodCallHandler(_handle);
+    channel.setMethodCallHandler(handle);
   }
 
-  Future _handle(MethodCall call) async {
+  Future handle(MethodCall call) async {
     switch (call.method) {
       case 'didReceiveMessage':
-        _messageStreamController.add(Map<String, dynamic>.from(call.arguments));
+        messageStreamController.add(Map<String, dynamic>.from(call.arguments));
         break;
       case 'didReceiveApplicationContext':
-        _contextStreamController.add(Map<String, dynamic>.from(call.arguments));
+        contextStreamController.add(Map<String, dynamic>.from(call.arguments));
         break;
       default:
         throw UnimplementedError('${call.method} not implemented');
@@ -67,17 +72,17 @@ abstract class WatchConnectivityBase {
   /// The most recently sent contextual data
   Future<Map<String, dynamic>> get applicationContext async {
     final applicationContext =
-        await channel.invokeMapMethod<String, dynamic>('applicationContext');
+    await channel.invokeMapMethod<String, dynamic>('applicationContext');
     return applicationContext ?? {};
   }
 
   /// A dictionary containing the last update data received
   Future<List<Map<String, dynamic>>> get receivedApplicationContexts async {
     final receivedApplicationContexts =
-        await channel.invokeListMethod<Map>('receivedApplicationContexts');
+    await channel.invokeListMethod<Map>('receivedApplicationContexts');
     return receivedApplicationContexts
-            ?.map((e) => e.cast<String, dynamic>())
-            .toList() ??
+        ?.map((e) => e.cast<String, dynamic>())
+        .toList() ??
         [];
   }
 
